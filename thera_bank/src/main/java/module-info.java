c7a837a08b04b335fc5aad102922eb4d2c517eb2
Alex T.H. <zelechos@hotmail.com>
2024-08-27 16:21:11 -0400

    feat(v0.0.0): config thera bank project

---
+++ thera_bank/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.bank.thera_bank {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.bank.thera_bank to javafx.fxml;
+    exports org.bank.thera_bank;
+}
